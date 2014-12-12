406b9b76deeae45be358f698246976d39e51ca73
filber <filber@foxmail.com>
2014-12-13 00:03:57 +0800

    修正了ReplaceMethodWithMethodObject.

--- Refactor/src/com/filber/refactor/_1_method/_08_ReplaceMethodWithMethodObject_160.java
+++ Refactor/src/com/filber/refactor/_1_method/_08_ReplaceMethodWithMethodObject_160.java
@@ -3,10 +3,12 @@ package com.filber.refactor._1_method;
 public class _08_ReplaceMethodWithMethodObject_160 {
 	
 	int delta(){
+        //Do Something.
 		return 0;
 	}
-	
+    //-------------------------------------------------------------------------------------------------
 	int badGamma(int inputVal,int quantity,int yearToDate) {
+        //方法体较为复杂,不可重用.
 		int importantValue1 = (inputVal*quantity) + delta();
 		int importantValue2 = (inputVal*yearToDate)+100;
 		if ((yearToDate-importantValue1)>100) {
@@ -15,14 +17,14 @@ public class _08_ReplaceMethodWithMethodObject_160 {
 		int importantValue3 = importantValue2*7;
 		return importantValue3 - 2* importantValue1;
 	}
-	
-	
+
+    //-------------------------------------------------------------------------------------------------
 	int goodGamma(int inputVal,int quantity,int yearToDate) {
+        //将复杂的方法体替换为一个MethodObject对象.
+        //传入this是为了在MethodObject内调用delta().
 		return new Gamma(inputVal, quantity, yearToDate, this).compute();
 	}
-	
-	
-	//在MethodObject内进行重构.
+
 	static class Gamma{
 		int inputVal;
 		int quantity;
@@ -36,7 +38,6 @@ public class _08_ReplaceMethodWithMethodObject_160 {
 			this.yearToDate = yearToDate;
 			this.origin = origin;
 		}
-
 		private int calImportantValue2(int importantValue1){
 			int importantValue2 = (inputVal*yearToDate)+100;
 			if ((yearToDate-importantValue1)>100) {
@@ -44,7 +45,7 @@ public class _08_ReplaceMethodWithMethodObject_160 {
 			}
 			return importantValue2;
 		}
-		int compute(){
+		public int compute(){
 			//如果delta没有访问origin的额外数据,可以将其MoveMethod至Gamma.
 			int importantValue1 = (inputVal*quantity) + origin.delta();
 			int importantValue2 = calImportantValue2(importantValue1);
