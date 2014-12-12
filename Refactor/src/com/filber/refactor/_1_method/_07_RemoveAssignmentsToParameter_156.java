dcf470904e2e8c5f6a98d3be67f8a7ae1c284823
filber <filber@foxmail.com>
2014-12-12 23:56:44 +0800

    修正了RemoveAssignmentsToParameter.

--- Refactor/src/com/filber/refactor/_1_method/_07_RemoveAssignmentsToParameter_156.java
+++ Refactor/src/com/filber/refactor/_1_method/_07_RemoveAssignmentsToParameter_156.java
@@ -1,9 +1,11 @@
 package com.filber.refactor._1_method;
 
+/**
+ * 移除对参数的赋值.
+ */
 public class _07_RemoveAssignmentsToParameter_156 {
-
-	// 对参数赋值,混淆了inputVal的含义
 	int badDiscount(int inputVal,int quantity,int yearToDate){
+        // 对参数inputVal赋值,混淆了inputVal的含义
 		if (inputVal>50) {
 			inputVal -=2;
 		}
@@ -17,8 +19,8 @@ public class _07_RemoveAssignmentsToParameter_156 {
 	}
 
     //-------------------------------------------------------------------------------------------------
-	// 建立临时变量用于存放返回结果.
 	int goodDiscount(int inputVal,int quantity,int yearToDate){
+        // 建立临时变量result用于存放返回结果.
 		int result = inputVal;
 		if (inputVal>50) {
 			result -=2;
