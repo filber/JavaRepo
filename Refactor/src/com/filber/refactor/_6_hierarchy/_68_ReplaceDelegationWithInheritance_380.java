d581c533ff6a2fff48aef2abe11a4fc890cd5d4d
filber <filber@foxmail.com>
2014-12-12 14:50:55 +0800

    增加了新类.

---
+++ Refactor/src/com/filber/refactor/_6_hierarchy/_68_ReplaceDelegationWithInheritance_380.java
@@ -0,0 +1,39 @@
+package com.filber.refactor._6_hierarchy;
+
+public class _68_ReplaceDelegationWithInheritance_380 {
+
+    class Person{
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getLastName(){
+            return name.substring(name.lastIndexOf(' ')+1);
+        }
+    }
+
+    class OldEmployee{
+        private Person person;
+        public OldEmployee(Person person) {
+            this.person = person;
+        }
+        public String getName(){
+            return person.getName();
+        }
+        public void setName(String name){
+            person.setName(name);
+        }
+        public String toString() {
+            return "Emp: "+person.getLastName();
+        }
+    }
+
+    class Employee extends Person{
+        public String toString() {
+            return "Emp: "+getLastName();
+        }
+    }
+}
