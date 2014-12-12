9f5f3a0e761b763184a62e21280da7b63823a500
filber <filber@foxmail.com>
2014-12-12 23:38:42 +0800

    修正了InlineMethod.

--- Refactor/src/com/filber/refactor/_1_method/_02_InlineMethod_142.java
+++ Refactor/src/com/filber/refactor/_1_method/_02_InlineMethod_142.java
@@ -1,14 +1,25 @@
 package com.filber.refactor._1_method;
 
+/**
+ * 内联函数.
+ */
 public class _02_InlineMethod_142 {
     //非必要的间接性总是让人不舒服
-    class OldCase{
+    class BadCase{
         private int numberOfLateDeliveries;
         public int getRating(){
             return moreThanFiveLateDeliveries()?2:1;
         }
         private boolean moreThanFiveLateDeliveries(){
             return numberOfLateDeliveries>5;
+        }
+    }
+
+    //-------------------------------------------------------------------------------------------------
+    class GoodCase{
+        private int numberOfLateDeliveries;
+        public int getRating(){
+            return numberOfLateDeliveries>5?2:1;
         }
     }
 }
