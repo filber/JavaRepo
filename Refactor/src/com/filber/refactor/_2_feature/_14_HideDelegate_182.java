b3b4219bb21960500b8d856d926db3a220f4a6f7
filber <filber@foxmail.com>
2014-12-13 12:43:39 +0800

    修正了HideDelegate.

--- Refactor/src/com/filber/refactor/_2_feature/_14_HideDelegate_182.java
+++ Refactor/src/com/filber/refactor/_2_feature/_14_HideDelegate_182.java
@@ -1,28 +1,49 @@
 package com.filber.refactor._2_feature;
 
+/**
+ * 隐藏委托关系
+ */
 public class _14_HideDelegate_182 {
 
-	class Person{
-		String _name;
-		Department _department;
-		public Department getDepartment() {
-			return _department;
-		}
-		
-		//Delegate Method
-		//将getDepartment().getManager()通过本方法Hide Delegate
-		public Person getManager(){
-			return _department.getManager();
-		}
-	}
-	
-	class Department{
-		Person _manager;
-		public Department(Person _manager) {
-			this._manager = _manager;
-		}
-		public Person getManager() {
-			return _manager;
-		}
-	}
+    class OldCase{
+        void clientCode(){
+            new Person().getDepartment().getManager();//需要使用委托才可以得到Manager.
+        }
+        class Person{
+            String name;
+            Department department;
+            public Department getDepartment() {
+                return department;
+            }
+        }
+        class Department{
+            Person manager;
+            public Person getManager() {
+                return manager;
+            }
+        }
+    }
+
+    //-------------------------------------------------------------------------------------------------
+    class NewCase{
+        void clientCode(){
+            new Person().getManager();//HideDelegate后可以直接得到Manager.
+        }
+        class Person{
+            String name;
+            Department department;
+            public Department getDepartment() {
+                return department;
+            }
+            public Person getManager(){
+                return department.getManager();
+            }
+        }
+        class Department{
+            Person manager;
+            public Person getManager() {
+                return manager;
+            }
+        }
+    }
 }
