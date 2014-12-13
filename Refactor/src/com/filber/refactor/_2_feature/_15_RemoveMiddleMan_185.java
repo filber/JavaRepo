68c4b2df5d58c110c9a4f21453e4604afe641a51
filber <filber@foxmail.com>
2014-12-13 12:54:15 +0800

    修正了RemoveMiddleMan.

--- Refactor/src/com/filber/refactor/_2_feature/_15_RemoveMiddleMan_185.java
+++ Refactor/src/com/filber/refactor/_2_feature/_15_RemoveMiddleMan_185.java
@@ -1,27 +1,79 @@
 package com.filber.refactor._2_feature;
 
+/**
+ * 移除中间人
+ */
 public class _15_RemoveMiddleMan_185 {
-    class Person{
-        String _name;
-        Department _department;
-        //让Client直接调用受托类.
-        public Department getDepartment() {
-            return _department;
+    class OldCase {
+        class Person {
+            String name;
+            Department department;
+
+            //Person中座了过多的简单委托动作.
+            public Person getManager() {
+                return department.getManager();
+            }
+
+            public double getAnnualBudget() {
+                return department.getAnnualBudget();
+            }
+
+            public int getStaffCount() {
+                return department.getStaffCount();
+            }
         }
 
-        //去除掉Middle Man
-//        public Person getManager(){
-//            return _department.getManager();
-//        }
+        class Department {
+            Person manager;
+
+            public Department(Person _manager) {
+                this.manager = _manager;
+            }
+
+            public Person getManager() {
+                return manager;
+            }
+
+            public double getAnnualBudget() {
+                return 0;
+            }
+
+            public int getStaffCount() {
+                return 0;
+            }
+        }
     }
 
-    class Department{
-        Person _manager;
-        public Department(Person _manager) {
-            this._manager = _manager;
+    //-------------------------------------------------------------------------------------------------
+    class NewCase {
+        class Person {
+            String name;
+            Department department;
+
+            //直接提供受托对象给Client调用.
+            public Department getDepartment() {
+                return department;
+            }
         }
-        public Person getManager() {
-            return _manager;
+
+        class Department {
+            Person manager;
+
+            public Department(Person _manager) {
+                this.manager = _manager;
+            }
+
+            public Person getManager() {
+                return manager;
+            }
+
+            public double getAnnualBudget() {
+                return 0;
+            }
+
+            public int getStaffCount() {
+                return 0;
+            }
         }
     }
 }
