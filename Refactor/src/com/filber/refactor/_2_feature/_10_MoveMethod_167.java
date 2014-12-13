264c3ef02d5ba27025ff34927c676d9904c602eb
filber <filber@foxmail.com>
2014-12-13 13:31:35 +0800

    整理了代码的格式.

--- Refactor/src/com/filber/refactor/_2_feature/_10_MoveMethod_167.java
+++ Refactor/src/com/filber/refactor/_2_feature/_10_MoveMethod_167.java
@@ -5,58 +5,62 @@ package com.filber.refactor._2_feature;
  */
 public class _10_MoveMethod_167 {
 
-    class BadCase{
+    class BadCase {
         class Account {
-            AccountType _type;
-            int _daysOverdrawn;
+            AccountType type;
+            int daysOverdrawn;
 
             //逻辑层面来说,overdraftCharge计算方式与具体的账户无关,与AccountType的关联更紧密.
             public double overdraftCharge() {
-                if (_type.isPremium()) {
+                if (type.isPremium()) {
                     double result = 10;
-                    if (_daysOverdrawn>7) {
-                        result += (_daysOverdrawn-7)*0.85;
+                    if (daysOverdrawn > 7) {
+                        result += (daysOverdrawn - 7) * 0.85;
                     }
                     return result;
                 } else {
-                    return _daysOverdrawn*1.75;
+                    return daysOverdrawn * 1.75;
                 }
             }
-            public double bankCharge(){
+
+            public double bankCharge() {
                 double result = 4.5;
-                if (_daysOverdrawn>0) {
+                if (daysOverdrawn > 0) {
                     result += overdraftCharge();
                 }
                 return result;
             }
         }
+
         class AccountType {
-            public boolean isPremium(){
+            public boolean isPremium() {
                 return false;
             }
         }
     }
 
     //-------------------------------------------------------------------------------------------------
-    class GoodCase{
+    class GoodCase {
         class Account {
-            AccountType _type;
-            int _daysOverdrawn;
+            AccountType type;
+            int daysOverdrawn;
 
             public double bankCharge() {
-                return 4.5 + _type.overdraftCharge(_daysOverdrawn);//消除了临时变量result
+                return 4.5 + type.overdraftCharge(daysOverdrawn);//消除了临时变量result
             }
         }
+
         class AccountType {
             //降低isPremium的访问级别为private,因为重构之后它已不被Account访问.
-            private boolean isPremium(){
+            private boolean isPremium() {
                 return false;
             }
+
             //搬移overdraftCharge至AccountType,并接收daysOverdrawn作为参数.
             public double overdraftCharge(int daysOverdrawn) {
-                if (daysOverdrawn<=0) return 0;//从Client中挪进来的逻辑.
-                if (!isPremium()) return daysOverdrawn*1.75;//将表达式的条件翻转,消除嵌套IF.
-                if (daysOverdrawn>7) return  10+(daysOverdrawn-7)*0.85;
+                if (daysOverdrawn <= 0) return 0;//从Client中挪进来的逻辑.
+                if (!isPremium()) return daysOverdrawn * 1.75;//将表达式的条件翻转,消除嵌套IF.
+                if (daysOverdrawn > 7) return 10 + (daysOverdrawn - 7) * 0.85;
                 return 10;
             }
         }
