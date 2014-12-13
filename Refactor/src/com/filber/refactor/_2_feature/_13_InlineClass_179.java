3dc9f1b25b899b266516da7a30f881c2dc193167
filber <filber@foxmail.com>
2014-12-13 12:36:34 +0800

    修正了ExtractClass.

--- Refactor/src/com/filber/refactor/_2_feature/_13_InlineClass_179.java
+++ Refactor/src/com/filber/refactor/_2_feature/_13_InlineClass_179.java
@@ -1,26 +1,50 @@
 package com.filber.refactor._2_feature;
 
+/**
+ * 将类内联化
+ */
 public class _13_InlineClass_179 {
+    class OldCase{
+        class Person {
+            String _name;
+            //TelephoneNumber可能只被Person使用,是一个萎缩类.
+            TelephoneNumber telephoneNumber;
+        }
+        class TelephoneNumber{
+            String _officeAreaCode;
+            String _officeNumber;
+            public String getTelephoneNumber() {
+                return "("+_officeAreaCode+") "+_officeNumber;
+            }
+        }
+    }
 
-	class BadPerson{
-		String _name;
-		TelephoneNumber telephoneNumber;
-	}
-	class TelephoneNumber{
-		String _officeAreaCode;
-		String _officeNumber;
-		public String getTelephoneNumber() {
-			return "("+_officeAreaCode+") "+_officeNumber;
-		}
-	}
-	
-	//将萎缩类TelephoneNumber塞进它的最频繁用户类Person中.
-	class GoodPerson{
-		String _name;
-		String _officeAreaCode;
-		String _officeNumber;
-		public String getTelephoneNumber() {
-			return "("+_officeAreaCode+") "+_officeNumber;
-		}
-	}
+    //-------------------------------------------------------------------------------------------------
+	//修正方式1:将萎缩类TelephoneNumber的属性和方法内联至它的唯一用户类Person中.
+    class NewCase1{
+        class Person {
+            String _name;
+            String _officeAreaCode;
+            String _officeNumber;
+            public String getTelephoneNumber() {
+                return "("+_officeAreaCode+") "+_officeNumber;
+            }
+        }
+    }
+
+    //-------------------------------------------------------------------------------------------------
+    //修正方式2:将萎缩类TelephoneNumber的作为内部类内联至唯一用户类Person中.避免外部访问.
+    class NewCase2{
+        class Person {
+            String _name;
+            TelephoneNumber telephoneNumber;
+            class TelephoneNumber{
+                String _officeAreaCode;
+                String _officeNumber;
+                public String getTelephoneNumber() {
+                    return "("+_officeAreaCode+") "+_officeNumber;
+                }
+            }
+        }
+    }
 }
