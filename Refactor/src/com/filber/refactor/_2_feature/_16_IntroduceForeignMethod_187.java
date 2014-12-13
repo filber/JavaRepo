498b057f90039e6dfe71089335babf84ddc397b5
filber <filber@foxmail.com>
2014-12-13 12:56:13 +0800

    修正了IntroduceForeignMethod.

--- Refactor/src/com/filber/refactor/_2_feature/_16_IntroduceForeignMethod_187.java
+++ Refactor/src/com/filber/refactor/_2_feature/_16_IntroduceForeignMethod_187.java
@@ -2,12 +2,16 @@ package com.filber.refactor._2_feature;
 
 import java.util.Date;
 
+/**
+ * 引入外加函数
+ */
 public class _16_IntroduceForeignMethod_187 {
     public static final Date previousEnd = new Date();
     public static final Date newStart = nextDay(previousEnd);
 
-    private static Date nextDay(Date arg){
+    //通过在Client中添加外加函数,扩充无法修改类的功能.
+    public static Date nextDay(Date arg) {
         //Foreign Method, should be on Date.
-        return new Date(arg.getYear(),arg.getMonth(),arg.getDay()+1);
+        return new Date(arg.getYear(), arg.getMonth(), arg.getDay() + 1);
     }
 }
