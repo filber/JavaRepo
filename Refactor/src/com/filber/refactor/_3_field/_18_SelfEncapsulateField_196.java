44906adf82068f2c87071e14512bef8f60fd86ad
filber <filber@foxmail.com>
2014-12-14 11:41:50 +0800

    Update Replace Data Value With Object.

--- Refactor/src/com/filber/refactor/_3_field/_18_SelfEncapsulateField_196.java
+++ Refactor/src/com/filber/refactor/_3_field/_18_SelfEncapsulateField_196.java
@@ -5,71 +5,75 @@ package com.filber.refactor._3_field;
  */
 public class _18_SelfEncapsulateField_196 {
 
-    class BadIntRange {
-        // 字段以public暴露给外部.
-        public int low, high;
+    class OldCase{
+        class IntRange {
+            // 字段以public暴露给外部.
+            public int low, high;
 
-        boolean includes(int arg) {
-            return arg >= low && arg <= high;
-        }
+            boolean includes(int arg) {
+                return arg >= low && arg <= high;
+            }
 
-        void grow(int factor) {
-            high = factor * high;
+            void grow(int factor) {
+                high = factor * high;
+            }
         }
     }
 
     //-------------------------------------------------------------------------------------------------
-    class GoodIntRange {
-        private int low, high;
+    class NewCase{
+        class IntRange {
+            private int low, high;
 
-        public GoodIntRange(int low, int high) {
-            //在构造函数中要谨慎使用Setter&Getter
-            //因为Setter&Getter通常被认为对象创建之后才使用.
-            this.low = low;
-            this.high = high;
+            public IntRange(int low, int high) {
+                //在构造函数中要谨慎使用Setter&Getter
+                //因为Setter&Getter通常被认为对象创建之后才使用.
+                this.low = low;
+                this.high = high;
+            }
+
+            //除Constructor以外的方法尽量使用Setter&Getter操作字段.
+            boolean includes(int arg) {
+                return arg >= getLow() && arg <= getHigh();
+            }
+
+            void grow(int factor) {
+                setHigh(factor * getHigh());
+            }
+
+            public int getLow() {
+                return low;
+            }
+
+            public void setLow(int low) {
+                this.low = low;
+            }
+
+            public int getHigh() {
+                return high;
+            }
+
+            public void setHigh(int high) {
+                this.high = high;
+            }
         }
 
-        //除Constructor以外的方法尽量使用Setter&Getter操作字段.
-        boolean includes(int arg) {
-            return arg >= getLow() && arg <= getHigh();
-        }
+        class CappedRange extends IntRange {
+            //CappedRange实例一旦创建则cap即不可更改.
+            private final int cap;
 
-        void grow(int factor) {
-            setHigh(factor * getHigh());
-        }
-
-        public int getLow() {
-            return low;
-        }
-
-        public void setLow(int low) {
-            this.low = low;
-        }
-
-        public int getHigh() {
-            return high;
-        }
-
-        public void setHigh(int high) {
-            this.high = high;
-        }
-    }
-
-    class CappedRange extends GoodIntRange {
-        //CappedRange实例一旦创建则cap即不可更改.
-        private final int cap;
-
-        public CappedRange(int low, int high, int cap) {
-            super(low, high);
-            this.cap = cap;
-        }
-        //cap只暴露出来了Getter
-        public int getCap() {
-            return cap;
-        }
-        //Override了继承自父类的getHigh
-        public int getHigh() {
-            return Math.min(super.getHigh(), getCap());
+            public CappedRange(int low, int high, int cap) {
+                super(low, high);
+                this.cap = cap;
+            }
+            //cap只暴露出来了Getter
+            public int getCap() {
+                return cap;
+            }
+            //Override了继承自父类的getHigh
+            public int getHigh() {
+                return Math.min(super.getHigh(), getCap());
+            }
         }
     }
 }
--- Refactor/src/com/filber/refactor/_3_field/_19_ReplaceDataValueWithObject_200.java
+++ Refactor/src/com/filber/refactor/_3_field/_19_ReplaceDataValueWithObject_200.java
@@ -3,67 +3,78 @@ package com.filber.refactor._3_field;
 import java.util.Collection;
 import java.util.Iterator;
 
-//对数据项产生了额外的行为
-//Duplicate Code
-//Feature Envy
+/**
+ * 以对象取代数值
+ */
 public class _19_ReplaceDataValueWithObject_200 {
 
-		interface Order{
-			String getCustomer();
-			void setCustomer(String customer);
-		}
-		class OldOrder implements Order{
-			public OldOrder(String customer) {
-				this._customer = customer;
-			}
-			public String getCustomer() {
-				return _customer;
-			}
-			public void setCustomer(String customer) {
-				this._customer = customer;
-			}
-			String _customer;
-		}
-		
-		//Customer为值对象,本身是没有Setter的
-		//可以额外追加行为,但不适于追加信用等级,地址之类的属性.
-		//使用ChangeValueToReference才可以.
-		class Customer{
-			final String _name;
-			public Customer(String _name) {
-				this._name = _name;
-			}
-			public String getName() {
-				return _name;
-			}
-			//值对象一般需要重写hashCode和equals
-			public int hashCode() {
-				return _name.hashCode();
-			}
-			//值对象的同一性体现于内部值是否相等,而非是否是一个对象.
-			public boolean equals(Object obj) {
-				return obj instanceof Customer&& getName().equals(((Customer)obj).getName());
-			}
-		}
-		class GoodOrder implements Order{
-			private Customer customer;
-			public String getCustomer() {
-				return customer.getName();
-			}
-			public void setCustomer(String customer) {
-				this.customer = new Customer(customer);
-			}
-		}
-		
-		static int numberOfOrdersFor(Collection<Order> orders, String customer){
-			int result = 0;
-			Iterator<Order> orderIterator = orders.iterator();
-			while (orderIterator.hasNext()) {
-				Order order = orderIterator.next();
-				if (order.getCustomer().equals(customer)) {
-					result ++;
-				}
-			}
-			return result;
-		}
+    static int numberOfOrdersFor(Collection<Order> orders, String customer) {
+        int result = 0;
+        Iterator<Order> orderIterator = orders.iterator();
+        while (orderIterator.hasNext()) {
+            Order order = orderIterator.next();
+            if (order.getCustomer().equals(customer)) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    interface Order {
+        String getCustomer();
+        void setCustomer(String customer);
+    }
+
+    //-------------------------------------------------------------------------------------------------
+    class OldOrder implements Order {
+        String customer;
+
+        public OldOrder(String customer) {
+            this.customer = customer;
+        }
+
+        public String getCustomer() {
+            return customer;
+        }
+
+        public void setCustomer(String customer) {
+            this.customer = customer;
+        }
+    }
+
+    //Customer为值对象,没有Setter的
+    //可以追加Method,但不适于追加信用等级,地址之类的可变属性.
+    class Customer {
+        final String name;
+
+        public Customer(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        //值对象一般需要重写hashCode和equals
+        public int hashCode() {
+            return name.hashCode();
+        }
+
+        //值对象的同一性体现于内部值是否相等,而非是否是一个对象.
+        public boolean equals(Object obj) {
+            return obj instanceof Customer && getName().equals(((Customer) obj).getName());
+        }
+    }
+
+    class GoodOrder implements Order {
+        private Customer customer;
+
+        public String getCustomer() {
+            return customer.getName();
+        }
+
+        public void setCustomer(String customer) {
+            this.customer = new Customer(customer);
+        }
+    }
 }
