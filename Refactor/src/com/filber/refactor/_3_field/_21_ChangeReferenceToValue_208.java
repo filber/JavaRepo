a28337bf61b5d3e5548e7891c96c5796e26f3010
filber <filber@foxmail.com>
2014-12-14 12:24:12 +0800

    Update Change Reference To Value.

--- Refactor/src/com/filber/refactor/_3_field/_21_ChangeReferenceToValue_208.java
+++ Refactor/src/com/filber/refactor/_3_field/_21_ChangeReferenceToValue_208.java
@@ -1,53 +1,56 @@
 package com.filber.refactor._3_field;
 
-//没有Setter,不可变(Immutable)
+/**
+ * 将引用对象改为值对象
+ */
 public class _21_ChangeReferenceToValue_208 {
 
-	static class OldCurrency{
-		//通过本工厂方法取得实例
-		public static OldCurrency getCurrency(String code) {
-			return new OldCurrency(code);
-		}
+    static class ReferenceCurrency {
+        private String code;
 
-		private String _code;
-		//<init>为private,不可直接创建实例
-		private OldCurrency(String code){
-			_code = code;
-		}
-		public String getCode(){
-			return _code;
-		}
-	}
-	
-	class NewCurrency{
-		private String _code;
+        private ReferenceCurrency(String code) {
+            this.code = code;
+        }
 
-		//<init>为public,可直接创建实例
-		public NewCurrency(String _code) {
-			this._code = _code;
-		}
+        //通过工厂方法取得实例
+        public static ReferenceCurrency getCurrency(String code) {
+            return new ReferenceCurrency(code);
+        }
 
-        //只有Getter,没有Setter
-        public String getCode(){
-			return _code;
-		}
-		
-		//TODO 对于值对象,hashCode的重写规则一般是所有字段的异或运算结果.
-		//field1.hashCode() ^ field2.hashCode()
-		public int hashCode() {
-			return _code.hashCode();
-		}
+        public String getCode() {
+            return code;
+        }
+    }
 
-		//TODO 对于值对象,equals的重写规则一般为
-		//filed1.equals(obj.field1) && field2.equals(obj.field2)
-		public boolean equals(Object obj) {
-			if(obj instanceof NewCurrency) {
-				NewCurrency instance = (NewCurrency)obj;
-				return getCode().equals(instance.getCode());
-			} else {
-				return false;
-			}
-		}
-		
-	}
+    //-------------------------------------------------------------------------------------------------
+    static class ValueCurrency {
+        private final String code;
+
+        public ValueCurrency(String code) {
+            if (code == null || code.isEmpty()) throw new IllegalArgumentException();
+            this.code = code;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        //重写hashCode与equals!!
+        //1.对于值对象,hashCode的重写规则一般是所有字段的^(异或运算)
+        //field1.hashCode() ^ field2.hashCode()
+        public int hashCode() {
+            return code.hashCode();
+        }
+
+        //2.对于值对象,equals的重写规则一般是所有字段的equals返回值&(与运算)
+        //filed1.equals(obj.field1) && field2==obj.field2
+        public boolean equals(Object obj) {
+            if (obj instanceof ValueCurrency) {
+                ValueCurrency instance = (ValueCurrency) obj;
+                return getCode().equals(instance.getCode());
+            } else {
+                return false;
+            }
+        }
+    }
 }
