cf905b92b50fcd3a30707122b27b8c3ffc881fe4
filber <filber@foxmail.com>
2014-12-14 12:42:10 +0800

    Update Replace Array With Object.

--- Refactor/src/com/filber/refactor/_3_field/_22_ReplaceArrayWithObject_211.java
+++ Refactor/src/com/filber/refactor/_3_field/_22_ReplaceArrayWithObject_211.java
@@ -1,48 +1,53 @@
 package com.filber.refactor._3_field;
 
+/**
+ * 以对象取代数值
+ */
 public class _22_ReplaceArrayWithObject_211 {
-	
-	class ArrayExample{
-		void test(){
-			String[] row = new String[3];
-			row[0]="liverpool";	//Team
-			row[1]="15";				//Win
-			row[2]="3";					//Loose
-			
-			//Client Code
-			String name = row[0];
-			int wins = Integer.valueOf(row[1]);
-			int looses = Integer.valueOf(row[2]);
-		}
-	}
-	
-	static class ObjectExample{
-		static class Performance{
-			String name;
-			int wins;
-			int looses;
-			public String getName() {
-				return name;
-			}
-			public int getWins() {
-				return wins;
-			}
-			public int getLooses() {
-				return looses;
-			}
-			public Performance(String name, int wins, int looses) {
-				this.name = name;
-				this.wins = wins;
-				this.looses = looses;
-			}
-		}
-		
-		void test(){
-			Performance performance = new Performance("liverpool", 15, 3);
-			//Client Code
-			String name = performance.getName();
-			int wins = performance.getWins();
-			int looses = performance.getLooses();
-		}
-	}
+    static class BadCase {
+        void test() {
+            String[] row = new String[3];
+            row[0] = "liverpool";    //Team
+            row[1] = "15";                //Win
+            row[2] = "3";                    //Loose
+
+            String name = row[0];
+            int wins = Integer.valueOf(row[1]);
+            int looses = Integer.valueOf(row[2]);
+        }
+    }
+
+    //-------------------------------------------------------------------------------------------------
+    static class GoodCase {
+        void test() {
+            Performance performance = new Performance("liverpool", 15, 3);
+            String name = performance.getName();
+            int wins = performance.getWins();
+            int looses = performance.getLooses();
+        }
+
+        static class Performance {
+            private final String name;
+            private final int wins;
+            private final int looses;
+
+            public Performance(String name, int wins, int looses) {
+                this.name = name;
+                this.wins = wins;
+                this.looses = looses;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public int getWins() {
+                return wins;
+            }
+
+            public int getLooses() {
+                return looses;
+            }
+        }
+    }
 }
