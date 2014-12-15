908db60a773876a77434c5db8dd9d512b6695268
filber <filber@foxmail.com>
2014-12-15 17:00:50 +0800

    Update Replace Record With Data Class.

--- Refactor/src/com/filber/refactor/_3_field/_29_ReplaceRecordWithDataClass_242.java
+++ Refactor/src/com/filber/refactor/_3_field/_29_ReplaceRecordWithDataClass_242.java
@@ -1,8 +1,56 @@
 package com.filber.refactor._3_field;
 
 /**
- * Created by Administrator on 2014/12/2.
+ * 以数据类取代记录
  */
 public class _29_ReplaceRecordWithDataClass_242 {
-    //这种"哑"数据对象POJO,随着重构的进行会被进一步赋予额外的行为
+
+    class BadCase {
+        //"哑"数据对象
+        public class BindCardInfo {
+            private String bindId;//绑卡ID
+            private String deleteFlag;////删除标识0正常 1删除
+
+            public BindCardInfo(String bindId, String deleteFlag) {
+                this.bindId = bindId;
+                this.deleteFlag = deleteFlag;
+            }
+
+            public String getBindId() {
+                return bindId;
+            }
+
+            public String getDeleteFlag() {
+                return deleteFlag;
+            }
+        }
+    }
+
+    //------------------------------------------------------------------
+    class GoodCase {
+        public class BindCardInfo {
+            public static final String DELETED = "1";
+
+            private String bindId;//绑卡ID
+            private String deleteFlag;////删除标识0正常 1删除
+
+            public BindCardInfo(String bindId, String deleteFlag) {
+                this.bindId = bindId;
+                this.deleteFlag = deleteFlag;
+            }
+
+            public String getBindId() {
+                return bindId;
+            }
+
+            public String getDeleteFlag() {
+                return deleteFlag;
+            }
+
+            //通过重构进一步赋予额外的行为
+            public boolean isDeleted() {
+                return DELETED.equals(getDeleteFlag());
+            }
+        }
+    }
 }
