f257ca9907362cfaf5b2efbb37d616f7883087fe
filber <filber@foxmail.com>
2014-12-15 16:30:21 +0800

    Update ReplaceMagicNumberWithSymbolicConstant.

--- Refactor/src/com/filber/refactor/_3_field/_26_ReplaceMagicNumberWithSymbolicConstant_229.java
+++ Refactor/src/com/filber/refactor/_3_field/_26_ReplaceMagicNumberWithSymbolicConstant_229.java
@@ -1,8 +1,20 @@
 package com.filber.refactor._3_field;
 
 /**
- * Created by Administrator on 2014/12/2.
+ * 以字面常量取代魔法数
  */
 public class _26_ReplaceMagicNumberWithSymbolicConstant_229 {
+    class BadCase {
+        double potentialEnergy(double mass, double height) {
+            return mass * 9.81 * height;
+        }
+    }
 
+    class GoodCase {
+        double potentialEnergy(double mass, double height) {
+            return mass * GRAVATATIONAL_CONSTANT * height;
+        }
+
+        static final double GRAVATATIONAL_CONSTANT = 9.81;
+    }
 }
