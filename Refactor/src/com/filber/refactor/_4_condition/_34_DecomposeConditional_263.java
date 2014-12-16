1c7190410699427dd2ef9b7e60d3e17b632e8f57
filber <filber@foxmail.com>
2014-12-16 19:04:24 +0800

    Update Consolidate Conditional Expression.

--- Refactor/src/com/filber/refactor/_4_condition/_34_DecomposeConditional_263.java
+++ Refactor/src/com/filber/refactor/_4_condition/_34_DecomposeConditional_263.java
@@ -3,7 +3,7 @@ package com.filber.refactor._4_condition;
 import java.util.Date;
 
 /**
- * Created by Administrator on 2014/12/3.
+ * 分解条件表达式
  */
 public class _34_DecomposeConditional_263 {
     public static final Date SUMMER_START = new Date();
@@ -13,31 +13,33 @@ public class _34_DecomposeConditional_263 {
     private double winterServiceCharge;
     private double summerRate;
 
-    public double badCalCharge(Date date,int quantity){
+    public double badCalCharge(Date date, int quantity) {
         double charge;
-        if (date.before(SUMMER_START)||date.after(SUMMER_END)){
-            charge = quantity*winterRate+winterServiceCharge;
+        if (date.before(SUMMER_START) || date.after(SUMMER_END)) {
+            charge = quantity * winterRate + winterServiceCharge;
         } else {
-            charge = quantity*summerRate;
+            charge = quantity * summerRate;
         }
         return charge;
     }
 
     //-------------------------------------------------------------------------------------------------
     //将if,then,else分别提取为独立的函数
+    private boolean notSummer(Date date) {
+        return date.before(SUMMER_START) || date.after(SUMMER_END);
+    }
 
-    private boolean notSummer(Date date){
-        return date.before(SUMMER_START)||date.after(SUMMER_END);
+    private double winterCharge(int quantity) {
+        return quantity * winterRate + winterServiceCharge;
     }
-    private double winterCharge(int quantity){
-        return quantity*winterRate+winterServiceCharge;
+
+    private double summerCharge(int quantity) {
+        return quantity * summerRate;
     }
-    private double summerCharge(int quantity){
-        return quantity*summerRate;
-    }
-    public double goodCalCharge(Date date,int quantity){
+
+    public double goodCalCharge(Date date, int quantity) {
         double charge;
-        if (notSummer(date)){
+        if (notSummer(date)) {
             charge = winterCharge(quantity);
         } else {
             charge = summerCharge(quantity);
--- Refactor/src/com/filber/refactor/_4_condition/_35_ConsolidateConditionalExpression_265.java
+++ Refactor/src/com/filber/refactor/_4_condition/_35_ConsolidateConditionalExpression_265.java
@@ -1,28 +1,27 @@
 package com.filber.refactor._4_condition;
 
 /**
- * Created by Administrator on 2014/12/3.
+ * 合并条件表达式
  */
 public class _35_ConsolidateConditionalExpression_265 {
-
     private int seniority;
     private int monthDisabled;
     private boolean isPartTime;
 
-    public double badDisabilityAmount(){
-        if (seniority<2)return 0;
-        if (monthDisabled>12)return 0;
+    public double badDisabilityAmount() {
+        if (seniority < 2) return 0;
+        if (monthDisabled > 12) return 0;
         if (isPartTime) return 0;
         return 1;
     }
 
     //-------------------------------------------------------------------------------------------------
-
-    private boolean notEligibleForDisability(){
-        return seniority<2||monthDisabled>12||isPartTime;
+    private boolean notEligibleForDisability() {
+        return seniority < 2 || monthDisabled > 12 || isPartTime;
     }
-    public double goodDisabilityAmount(){
-        if(notEligibleForDisability()){
+
+    public double goodDisabilityAmount() {
+        if (notEligibleForDisability()) {
             return 0;
         } else {
             return 1;
@@ -30,22 +29,20 @@ public class _35_ConsolidateConditionalExpression_265 {
     }
 
     //-------------------------------------------------------------------------------------------------
-    public double badNestedCondition(){
-        if (isPartTime){
-            if (seniority<2){
+    public double badNestedCondition() {
+        if (isPartTime) {
+            if (seniority < 2) {
                 return 0;
             }
         }
         return 0.5;
     }
 
-    public double goodNestedCondition(){
-        if (isPartTime && seniority<2){
+    //------------------------------------------------------------------
+    public double goodNestedCondition() {
+        if (isPartTime && seniority < 2) {
             return 0;
         }
         return 0.5;
     }
-    //-------------------------------------------------------------------------------------------------
-
-
 }
