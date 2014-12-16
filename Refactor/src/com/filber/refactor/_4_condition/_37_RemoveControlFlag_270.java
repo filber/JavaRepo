95c923f11380307e98a5b7a3253fc6011db22d05
filber <filber@foxmail.com>
2014-12-16 20:10:02 +0800

    Update Remove Control Flag.

--- Refactor/src/com/filber/refactor/_4_condition/_37_RemoveControlFlag_270.java
+++ Refactor/src/com/filber/refactor/_4_condition/_37_RemoveControlFlag_270.java
@@ -1,52 +1,51 @@
 package com.filber.refactor._4_condition;
 
 /**
- * Created by Administrator on 2014/12/3.
+ * 移除控制标记
  */
 public class _37_RemoveControlFlag_270 {
 
-    private void sendAlert(){}
-
-    public void badCheckSecurity(String[] people){
+    public void badCheckSecurity(String[] people) {
         boolean found = false;
         for (int i = 0; i < people.length; i++) {
-            if (!found){
-                if ("Don".equals(people[i])){
-                    found=true;
+            if (!found) {
+                if ("Don".equals(people[i])) {
+                    found = true;
                     sendAlert();
                 }
-                if ("John".equals(people[i])){
-                    found=true;
+                if ("John".equals(people[i])) {
+                    found = true;
                     sendAlert();
                 }
             }
         }
     }
+
     //-------------------------------------------------------------------------------------------------
-    private boolean isBadPerson(String person){
-        return "Don".equals(person) || "John".equals(person);
-    }
     //以break取代Control Flag
-    public void breakCheckSecurity(String[] people){
+    public void breakCheckSecurity(String[] people) {
         for (int i = 0; i < people.length; i++) {
-            if (isBadPerson(people[i])){
+            if (isBadPerson(people[i])) {
                 sendAlert();
                 break;
             }
         }
     }
-    //-------------------------------------------------------------------------------------------------
 
-    private void someLaterCode(String person){}
-    public void bad2CheckSecurity(String[] people){
+    private boolean isBadPerson(String person) {
+        return "Don".equals(person) || "John".equals(person);
+    }
+
+    //-------------------------------------------------------------------------------------------------
+    public void badReturnCheckSecurity(String[] people) {
         String found = "";
         for (int i = 0; i < people.length; i++) {
-            if ("".equals(found)){
-                if ("Don".equals(people[i])){
+            if ("".equals(found)) {
+                if ("Don".equals(people[i])) {
                     sendAlert();
                     found = "Don";
                 }
-                if ("John".equals(people[i])){
+                if ("John".equals(people[i])) {
                     sendAlert();
                     found = "John";
                 }
@@ -56,19 +55,25 @@ public class _37_RemoveControlFlag_270 {
     }
 
     //-------------------------------------------------------------------------------------------------
-
-    private String foundMiscreant(String[] people){
+    private String foundMiscreant(String[] people) {
         for (int i = 0; i < people.length; i++) {
-            if ("Don".equals(people[i])||"John".equals(people[i])){
+            if ("Don".equals(people[i]) || "John".equals(people[i])) {
                 sendAlert();
                 return people[i];
             }
         }
         return "";
     }
-    public void returnCheckSecurity(String[] people){
+
+    public void goodReturnCheckSecurity(String[] people) {
         String found = foundMiscreant(people);
         someLaterCode(found);
+    }
+
+    private void sendAlert() {
+    }
+
+    private void someLaterCode(String person) {
     }
 }
 
