aa9b48234d8344ec100a1c75719909b7a76f8321
filber <filber@foxmail.com>
2014-12-16 20:50:35 +0800

    Update Introduce Null Object.

--- Refactor/src/com/filber/refactor/_4_condition/_39_ReplaceConditionalWithPolymorphism_280.java
+++ Refactor/src/com/filber/refactor/_4_condition/_39_ReplaceConditionalWithPolymorphism_280.java
@@ -1,102 +1,126 @@
 package com.filber.refactor._4_condition;
 
 /**
- * Created by Administrator on 2014/12/3.
+ * 以多态取代条件表达式
  */
-public class _39_ReplaceConditionalWithPolymorphism_280{
+public class _39_ReplaceConditionalWithPolymorphism_280 {
 
-    public class OldBird {
-        public static final int EUROPEAN = 1;
-        public static final int AFRICAN = 2;
-        public static final int NORWEGIAN_BLUE = 3;
-        private int type;
-        private double voltage;
-        private int numberOfCoconuts;
-        private boolean isNailed;
+    class BadCase {
+        public class Bird {
+            public static final int EUROPEAN = 1;
+            public static final int AFRICAN = 2;
+            public static final int NORWEGIAN_BLUE = 3;
+            private int type;
+            private double voltage;
+            private int numberOfCoconuts;
+            private boolean isNailed;
 
-        private double getBaseSpeed(){return 0;}
-
-        private double getBaseSpeed(double voltage){return 0;}
-
-        private double getLoadFactor(){return 0;}
-
-        public double getWeight(){
-            switch (type){
-                case EUROPEAN:
-                    return 1.5;
-                case AFRICAN:
-                    return 1.2;
-                case NORWEGIAN_BLUE:
-                    return 1.8;
+            private double getBaseSpeed() {
+                return 0;
             }
-            throw new UnsupportedOperationException();
-        }
-        public double getSpeed(){
-            switch (type){
-                case EUROPEAN:
-                    return getBaseSpeed();
-                case AFRICAN:
-                    return getBaseSpeed() - getLoadFactor()*numberOfCoconuts;
-                case NORWEGIAN_BLUE:
-                    return isNailed?0:getBaseSpeed(voltage);
+
+            private double getBaseSpeed(double voltage) {
+                return 0;
             }
-            throw new UnsupportedOperationException();
+
+            private double getLoadFactor() {
+                return 0;
+            }
+
+            public double getWeight() {
+                switch (type) {
+                    case EUROPEAN:
+                        return 1.5;
+                    case AFRICAN:
+                        return 1.2;
+                    case NORWEGIAN_BLUE:
+                        return 1.8;
+                }
+                throw new UnsupportedOperationException();
+            }
+
+            public double getSpeed() {
+                switch (type) {
+                    case EUROPEAN:
+                        return getBaseSpeed();
+                    case AFRICAN:
+                        return getBaseSpeed() - getLoadFactor() * numberOfCoconuts;
+                    case NORWEGIAN_BLUE:
+                        return isNailed ? 0 : getBaseSpeed(voltage);
+                }
+                throw new UnsupportedOperationException();
+            }
         }
     }
+
     //-------------------------------------------------------------------------------------------------
-    static abstract class Bird{
-        public static final int EUROPEAN = 1;
-        public static final int AFRICAN = 2;
-        public static final int NORWEGIAN_BLUE = 3;
+    static class GoodCase {
+        static abstract class Bird {
+            public static final int EUROPEAN = 1;
+            public static final int AFRICAN = 2;
+            public static final int NORWEGIAN_BLUE = 3;
 
-        protected double getBaseSpeed(){return 0;}
-
-        public static Bird createBird(int type){
-            if (EUROPEAN==type){
-                return new EuropeanBird();
-            } else if(AFRICAN==type){
-                return new AfricanBird();
-            } else if(NORWEGIAN_BLUE==type){
-                return new NorwegianBlueBird();
+            protected double getBaseSpeed() {
+                return 0;
             }
-            throw new IllegalArgumentException();
+
+            public static Bird createBird(int type) {
+                if (EUROPEAN == type) {
+                    return new EuropeanBird();
+                } else if (AFRICAN == type) {
+                    return new AfricanBird();
+                } else if (NORWEGIAN_BLUE == type) {
+                    return new NorwegianBlueBird();
+                }
+                throw new IllegalArgumentException();
+            }
+
+            public abstract double getWeight();
+
+            public abstract double getSpeed();
         }
-        public abstract double getWeight();
-        public abstract double getSpeed();
-    }
-    static class EuropeanBird extends Bird {
-        @Override
-        public double getWeight() {
-            return 1.5;
+
+        static class EuropeanBird extends Bird {
+            public double getWeight() {
+                return 1.5;
+            }
+
+            public double getSpeed() {
+                return super.getBaseSpeed();
+            }
         }
-        @Override
-        public double getSpeed() {
-            return super.getBaseSpeed();
+
+        static class AfricanBird extends Bird {
+            private int numberOfCoconuts;
+
+            private double getLoadFactor() {
+                return 0;
+            }
+
+            public double getWeight() {
+                return 1.2;
+            }
+
+            public double getSpeed() {
+                return super.getBaseSpeed() - getLoadFactor() * numberOfCoconuts;
+            }
         }
-    }
-    static class AfricanBird extends Bird {
-        private int numberOfCoconuts;
-        private double getLoadFactor(){return 0;}
-        @Override
-        public double getWeight() {
-            return 1.2;
-        }
-        @Override
-        public double getSpeed() {
-            return super.getBaseSpeed() - getLoadFactor()*numberOfCoconuts;
-        }
-    }
-    static class NorwegianBlueBird extends Bird {
-        private boolean isNailed;
-        private double voltage;
-        private double getBaseSpeed(double voltage){return 0;}
-        @Override
-        public double getWeight() {
-            return 1.8;
-        }
-        @Override
-        public double getSpeed() {
-            return isNailed?0:getBaseSpeed(voltage);
+
+        static class NorwegianBlueBird extends Bird {
+            private boolean isNailed;
+            private double voltage;
+
+            private double getBaseSpeed(double voltage) {
+                return 0;
+            }
+
+            public double getWeight() {
+                return 1.8;
+            }
+
+            public double getSpeed() {
+                return isNailed ? 0 : getBaseSpeed(voltage);
+            }
         }
     }
 }
--- Refactor/src/com/filber/refactor/_4_condition/_40_IntroduceNullObject_285.java
+++ Refactor/src/com/filber/refactor/_4_condition/_40_IntroduceNullObject_285.java
@@ -1,111 +1,130 @@
 package com.filber.refactor._4_condition;
 
-import javax.lang.model.type.NullType;
-
 /**
- * Created by Administrator on 2014/12/4.
+ * 引入Null对象
  */
 public class _40_IntroduceNullObject_285 {
 
-    class Site {
-        private Customer customer;
-        public Customer getCustomer(){
-            if (customer==null)return Customer.createNull();
-            else return customer;
-        }
-    }
-    static class Customer {
-        private PaymentHistory history;
-        private BillingPlan plan;
-        public String getName(){
-            return "";
-        }
-        public BillingPlan getPlan() {
-            return plan;
-        }
-        public PaymentHistory getHistory() {
-            return history;
-        }
-        //在父类中增加isNull方法.
-        public boolean isNull(){
-            return false;
-        }
-        public static Customer createNull(){
-            return new NullCustomer();
-        }
-    }
-    static class PaymentHistory {
-        public static PaymentHistory createNull(){
-            return new NullPaymentHistory();
-        }
-        public int getWeeksDelinquentInLastYear(){
-            return 10;
-        }
-    }
-    static class BillingPlan {
-        public static BillingPlan basic(){
-            return new BillingPlan();
-        }
-    }
-    void oldCase(){
+    //------------------------------------------------------------------
+    void oldCase() {
         Site site = new Site();
         Customer customer = site.getCustomer();
         BillingPlan plan;
-        if (customer==null) plan = BillingPlan.basic();
-        else plan =customer.getPlan();
+        if (customer == null) plan = BillingPlan.basic();
+        else plan = customer.getPlan();
 
         String customerName;
-        if (customer==null)customerName="occupant";
+        if (customer == null) customerName = "occupant";
         else customerName = customer.getName();
 
         int weeksDelinquent;
-        if (customer==null)weeksDelinquent=0;
-        else weeksDelinquent=customer.getHistory().getWeeksDelinquentInLastYear();
+        if (customer == null) weeksDelinquent = 0;
+        else weeksDelinquent = customer.getHistory().getWeeksDelinquentInLastYear();
     }
 
-    //-------------------------------------------------------------------------------------------------
-    //第一个空对象
-    static class NullPaymentHistory extends PaymentHistory{
-        public int getWeeksDelinquentInLastYear() {
-            return 0;
-        }
-    }
-    interface Null{}
-    //第二个空对象
-    static class NullCustomer extends Customer implements Null{
-        public String getName() {
-            return "occupant";
-        }
-        public boolean isNull(){
-            return true;
-        }
-        public PaymentHistory getHistory() {
-            return PaymentHistory.createNull();
-        }
-        public BillingPlan getPlan() {
-            return BillingPlan.basic();
-        }
-    }
-
+    //------------------------------------------------------------------
     //通过空对象去除掉了对于Null的判断.
-    void newCase(){
+    void newCase() {
         Site site = new Site();
         Customer customer = site.getCustomer();
         BillingPlan plan;
         plan = customer.getPlan();
 
         String customerName;
-        customerName=customer.getName();
+        customerName = customer.getName();
 
         int weeksDelinquent;
         weeksDelinquent = customer.getHistory().getWeeksDelinquentInLastYear();
 
-        //1.对customer是否为空对象的判断
+        //两种方式判断customer是否为空对象
+        //1.通过多态
         if (customer.isNull()) ;
         else ;
 
-        //2.对customer是否为空对象的判断
+        //2.通过实例类型
         if (customer instanceof Null) ;
         else ;
+    }
+
+    //------------------------------------------------------------------
+    class Site {
+        private Customer customer;
+
+        public Customer getCustomer() {
+            if (customer == null) return Customer.createNull();
+            else return customer;
+        }
+    }
+
+    static class Customer {
+        private PaymentHistory history;
+        private BillingPlan plan;
+
+        public String getName() {
+            return "";
+        }
+
+        public BillingPlan getPlan() {
+            return plan;
+        }
+
+        public PaymentHistory getHistory() {
+            return history;
+        }
+
+        //在父类中增加isNull方法.
+        public boolean isNull() {
+            return false;
+        }
+
+        public static Customer createNull() {
+            return new NullCustomer();
+        }
+    }
+
+    static class PaymentHistory {
+        public static PaymentHistory createNull() {
+            return new NullPaymentHistory();
+        }
+
+        public int getWeeksDelinquentInLastYear() {
+            return 10;
+        }
+    }
+
+    static class BillingPlan {
+        public static BillingPlan basic() {
+            return new BillingPlan();
+        }
+    }
+
+    //-------------------------------------------------------------------------------------------------
+    //第一个空对象
+    static class NullPaymentHistory extends PaymentHistory {
+        public int getWeeksDelinquentInLastYear() {
+            return 0;
+        }
+    }
+
+    interface Null {
+    }
+
+    //第二个空对象
+    static class NullCustomer extends Customer implements Null {
+        public String getName() {
+            return "occupant";
+        }
+
+        public boolean isNull() {
+            return true;
+        }
+
+        public PaymentHistory getHistory() {
+            return PaymentHistory.createNull();
+        }
+
+        public BillingPlan getPlan() {
+            return BillingPlan.basic();
+        }
     }
 }
