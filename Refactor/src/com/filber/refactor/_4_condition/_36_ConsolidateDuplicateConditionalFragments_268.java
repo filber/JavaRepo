cda8641ed19a5aa1e9567b2c9c28a44060a0249f
filber <filber@foxmail.com>
2014-12-16 19:50:21 +0800

    Update Consolidate Duplicate Conditional Fragments.

--- Refactor/src/com/filber/refactor/_4_condition/_36_ConsolidateDuplicateConditionalFragments_268.java
+++ Refactor/src/com/filber/refactor/_4_condition/_36_ConsolidateDuplicateConditionalFragments_268.java
@@ -1,36 +1,42 @@
 package com.filber.refactor._4_condition;
 
 /**
- * Created by Administrator on 2014/12/3.
+ * 合并重复的条件片段
  */
 public class _36_ConsolidateDuplicateConditionalFragments_268 {
 
-    private void send(){
-
-    }
-
-    public double badOperation(double price,boolean isSpecialDeal){
-        double total;
-        if (isSpecialDeal){
-            total = 0.95*price;
-            send();
-        } else {
-            total = 0.98*price;
-            send();
+    class BadCase {
+        public double badFragments(double price, boolean isSpecialDeal) {
+            double total;
+            if (isSpecialDeal) {
+                total = 0.95 * price;
+                if (total > 1000) System.out.println("Total Exceed Limit.");
+            } else {
+                total = 0.98 * price;
+                if (total > 1000) System.out.println("Total Exceed Limit.");
+            }
+            return total;
         }
-        return total;
     }
+
     //-------------------------------------------------------------------------------------------------
-    // 通过将重复的代码片段搬移到外部
-    // 凸显出哪些东西随着条件变化而变化,哪些东西保持不变
-    public double goodOperation(double price,boolean isSpecialDeal){
-        double total;
-        if (isSpecialDeal){
-            total = 0.95*price;
-        } else {
-            total = 0.98*price;
+    class GoodCase {
+        // 通过将重复的代码片段搬移到外部
+        // 凸显出哪些东西随着条件变化而变化,哪些东西保持不变
+        public double goodOperation(double price, boolean isSpecialDeal) {
+            double total;
+            if (isSpecialDeal) {
+                total = 0.95 * price;
+            } else {
+                total = 0.98 * price;
+            }
+            alert(total);
+            return total;
         }
-        send();
-        return total;
+
+        private void alert(double total) {
+            if (total > 1000) System.out.println("Total Exceed Limit.");
+        }
     }
+
 }
