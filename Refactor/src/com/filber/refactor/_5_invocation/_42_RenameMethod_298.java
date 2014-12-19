613e33bc242c662009f345e7b7148d2113006442
filber <filber@foxmail.com>
2014-12-19 11:26:43 +0800

    Update Rename Method.

--- Refactor/src/com/filber/refactor/_5_invocation/_42_RenameMethod_298.java
+++ Refactor/src/com/filber/refactor/_5_invocation/_42_RenameMethod_298.java
@@ -1,24 +1,22 @@
 package com.filber.refactor._5_invocation;
 
 /**
- * Created by Administrator on 2014/12/5.
+ * 重命名函数
  */
 public class _42_RenameMethod_298 {
 
-    class Person{
+    class Person {
         private String officeAreaCode;
         private String officeNumber;
 
-        /**
-         * 将旧函数改为Delegate的方式引用新函数.
-         */
+        // 将旧函数改为Delegate的方式引用新函数.
         @Deprecated
-        public String getTelephoneNumber(){
+        public String getTelephoneNumber() {
             return getOfficeTelephoneNumber();
         }
 
-        public String getOfficeTelephoneNumber(){
-            return "("+officeAreaCode+")"+officeNumber;
+        public String getOfficeTelephoneNumber() {
+            return "(" + officeAreaCode + ")" + officeNumber;
         }
     }
 }
