2da5fb1e30c179b118883503792fa2ed212dc5d5
filber <filber@foxmail.com>
2014-12-19 11:50:27 +0800

    Update Replace Parameter with Method.

--- Refactor/src/com/filber/refactor/_5_invocation/_48_PreserveWholeObject_313.java
+++ Refactor/src/com/filber/refactor/_5_invocation/_48_PreserveWholeObject_313.java
@@ -4,59 +4,72 @@ package com.filber.refactor._5_invocation;
  * 保留对象完整
  */
 public class _48_PreserveWholeObject_313 {
-    static class BadCase{
-        static class TempRange{
+    class BadCase {
+        class TempRange {
             private int low;
             private int high;
+
             public int getLow() {
                 return low;
             }
+
             public int getHigh() {
                 return high;
             }
         }
-        static class HeatingPlan{
+
+        class HeatingPlan {
             private TempRange range;
-            public boolean withinRange(int low,int high){
-                if (low>=range.getLow()&&high<=range.getHigh())return true;
+
+            public boolean withinRange(int low, int high) {
+                if (low >= range.getLow() && high <= range.getHigh()) return true;
                 else return false;
             }
         }
-        static class Room{
+
+        class Room {
             private TempRange range;
-            public boolean withinPlan(HeatingPlan plan){
-                int low=range.getLow();
-                int high=range.getHigh();
-                return plan.withinRange(low,high);
+
+            public boolean withinPlan(HeatingPlan plan) {
+                int low = range.getLow();
+                int high = range.getHigh();
+                return plan.withinRange(low, high);
             }
         }
     }
 
-    static class GoodCase{
-        static class TempRange{
+    class GoodCase {
+        class TempRange {
             private int low;
             private int high;
+
             public int getLow() {
                 return low;
             }
+
             public int getHigh() {
                 return high;
             }
-            public boolean includes(TempRange range){
-                if (range.getLow()>= this.getLow()&&range.getHigh()<= this.getHigh())return true;
+
+            public boolean includes(TempRange range) {
+                if (range.getLow() >= this.getLow() && range.getHigh() <= this.getHigh()) return true;
                 else return false;
             }
         }
-        static class HeatingPlan{
+
+        class HeatingPlan {
             private TempRange range;
+
             //通过使用已有的对象缩减参数长度
-            public boolean withinRange(TempRange range){
+            public boolean withinRange(TempRange range) {
                 return this.range.includes(range);
             }
         }
-        static class Room{
+
+        class Room {
             private TempRange range;
-            public boolean withinPlan(HeatingPlan plan){
+
+            public boolean withinPlan(HeatingPlan plan) {
                 return plan.withinRange(range);//Delegate
             }
         }
