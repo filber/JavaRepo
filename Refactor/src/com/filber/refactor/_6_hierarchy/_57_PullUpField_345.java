00b03662e206e7078b006faea9d342336a279ba2
filber <filber@foxmail.com>
2014-12-22 15:41:56 +0800

    Update Pull Up Field.

--- Refactor/src/com/filber/refactor/_6_hierarchy/_57_PullUpField_345.java
+++ Refactor/src/com/filber/refactor/_6_hierarchy/_57_PullUpField_345.java
@@ -1,25 +1,44 @@
 package com.filber.refactor._6_hierarchy;
 
 /**
- * Created by Administrator on 2014/12/10.
+ * 字段上移
  */
 public class _57_PullUpField_345 {
-    class OldCase{
-        class Employee{}
-        class Salesman{
-            private String name;
+    class BadCase {
+        class Employee {
         }
-        class Engineer{
+
+        class Salesman {
             private String name;
+
+            public String getSalesmanName() {
+                return name;
+            }
+        }
+
+        class Engineer {
+            private String name;
+
+            public String getEngineerName() {
+                return name;
+            }
         }
     }
 
-    //将子类中private的属性在父类中声明为protected.
-    class NewCase{
-        class Employee{
-            protected String name;
+    //------------------------------------------------------------------
+    class GoodCase {
+        class Employee {
+            private String name;
+
+            public String getName() {
+                return name;
+            }
         }
-        class Salesman{}
-        class Engineer{}
+
+        class Salesman {
+        }
+
+        class Engineer {
+        }
     }
 }
