3b5bee82e4a618c9b6177d12fd7dae7c8677c566
filber <filber@foxmail.com>
2014-12-22 17:29:40 +0800

    Update Extract Hierarchy.

--- Refactor/src/com/filber/refactor/_7_huge/_72_ExtractHierarchy_400.java
+++ Refactor/src/com/filber/refactor/_7_huge/_72_ExtractHierarchy_400.java
@@ -1,14 +1,32 @@
 package com.filber.refactor._7_huge;
 
+/**
+ * 提炼继承体系
+ */
 public class _72_ExtractHierarchy_400 {
-    class BadCase{
-        class BillingScheme{
-            private boolean isDisabilityScheme(){
+    class BadCase {
+        class BillingScheme {
+            private boolean isDisabilityScheme() {
                 if (true) return true;
                 else return false;
             }
-            public void createSchema(){
-                if (isDisabilityScheme()){
+
+            private boolean isBusinessScheme() {
+                if (true) return true;
+                else return false;
+            }
+
+            private boolean isResidentialScheme() {
+                if (true) return true;
+                else return false;
+            }
+
+            public void createSchema() {
+                if (isDisabilityScheme()) {
+                    //do something
+                } else if (isBusinessScheme()) {
+                    //do something
+                } else if (isResidentialScheme()) {
                     //do something
                 } else {
                     //xxxx
@@ -17,11 +35,25 @@ public class _72_ExtractHierarchy_400 {
         }
     }
 
-    class GoodCase{
-        abstract class BillingScheme{
+    //------------------------------------------------------------------
+    class GoodCase {
+        abstract class BillingScheme {
             public abstract void createSchema();
         }
-        class DisableBillingScheme extends BillingScheme{
+
+        class DisableBillingScheme extends BillingScheme {
+            public void createSchema() {
+                //do something
+            }
+        }
+
+        class BusinessBillingScheme extends BillingScheme {
+            public void createSchema() {
+                //do something
+            }
+        }
+
+        class ResidentialBillingScheme extends BillingScheme {
             public void createSchema() {
                 //do something
             }
