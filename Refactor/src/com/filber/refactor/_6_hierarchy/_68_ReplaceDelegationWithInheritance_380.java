39f23cf97a1c5d52d33021a4b349ce9240031670
filber <filber@foxmail.com>
2014-12-22 16:57:02 +0800

    Update Replace Delegation with Inheritance.

--- Refactor/src/com/filber/refactor/_6_hierarchy/_68_ReplaceDelegationWithInheritance_380.java
+++ Refactor/src/com/filber/refactor/_6_hierarchy/_68_ReplaceDelegationWithInheritance_380.java
@@ -1,42 +1,57 @@
 package com.filber.refactor._6_hierarchy;
 
+/**
+ * 以继承取代委托
+ */
 public class _68_ReplaceDelegationWithInheritance_380 {
+    //两个类之间存在太多的委托关系.
 
-    //Test Conflict.
-    //Second Test Conflict.
-
-    class Person{
+    class Person {
         private String name;
+
         public String getName() {
             return name;
         }
+
         public void setName(String name) {
             this.name = name;
         }
-        public String getLastName(){
-            return name.substring(name.lastIndexOf(' ')+1);
+
+        public String getLastName() {
+            return name.substring(name.lastIndexOf(' ') + 1);
         }
     }
 
-    class OldEmployee{
-        private Person person;
-        public OldEmployee(Person person) {
-            this.person = person;
-        }
-        public String getName(){
-            return person.getName();
-        }
-        public void setName(String name){
-            person.setName(name);
-        }
-        public String toString() {
-            return "Emp: "+person.getLastName();
+    //------------------------------------------------------------------
+    class DelegationCase {
+        class Employee {
+            private Person person;
+
+            public Employee(Person person) {
+                this.person = person;
+            }
+
+            public String getName() {
+                return person.getName();
+            }
+
+            public void setName(String name) {
+                person.setName(name);
+            }
+
+            public String toString() {
+                return "Emp: " + person.getLastName();
+            }
         }
     }
 
-    class Employee extends Person{
-        public String toString() {
-            return "Emp: "+getLastName();
+    //------------------------------------------------------------------
+    class InheritanceCase {
+        class Employee extends Person {
+            public String toString() {
+                return "Emp: " + getLastName();
+            }
         }
     }
+
 }
