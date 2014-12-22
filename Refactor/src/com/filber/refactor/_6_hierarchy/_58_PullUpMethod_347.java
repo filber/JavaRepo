8e318bc7f6f2f94a687ef88881b31cd7b9c8c749
filber <filber@foxmail.com>
2014-12-22 15:49:00 +0800

    Update Pull Up Method.

--- Refactor/src/com/filber/refactor/_6_hierarchy/_58_PullUpMethod_347.java
+++ Refactor/src/com/filber/refactor/_6_hierarchy/_58_PullUpMethod_347.java
@@ -3,55 +3,65 @@ package com.filber.refactor._6_hierarchy;
 import java.util.Date;
 
 /**
- * Created by Administrator on 2014/12/10.
+ * 函数上移
  */
 public class _58_PullUpMethod_347 {
-    class OldCase{
-        class Customer{
+    class BadCase {
+        class Customer {
             protected Date lastBillDate;
-            protected void addBill(Date date,Double amount){
+
+            protected void addBill(Date date, Double amount) {
             }
         }
-        class RegularCustomer extends Customer{
-            public void createBill(Date date){
-                double chargeAmount=chargeFor(lastBillDate,date);
-                addBill(date,chargeAmount);
+
+        class RegularCustomer extends Customer {
+            public void createBill(Date date) {
+                double chargeAmount = chargeFor(lastBillDate, date);
+                addBill(date, chargeAmount);
             }
-            private double chargeFor(Date start,Date end){
+
+            private double chargeFor(Date start, Date end) {
                 return 2.0;
             }
         }
-        class PreferredCustomer extends Customer{
-            public void createBill(Date date){
-                double chargeAmount=chargeFor(lastBillDate,date);
-                addBill(date,chargeAmount);
+
+        class PreferredCustomer extends Customer {
+            public void createBill(Date date) {
+                double chargeAmount = chargeFor(lastBillDate, date);
+                addBill(date, chargeAmount);
             }
-            private double chargeFor(Date start,Date end){
+
+            private double chargeFor(Date start, Date end) {
                 return 1.0;
             }
         }
     }
 
     //-------------------------------------------------------------------------------------------------
-
-    class NewCase{
-        abstract class Customer{
+    class GoodCase {
+        abstract class Customer {
             protected Date lastBillDate;
-            protected void addBill(Date date,Double amount){}
-            public void createBill(Date date){
-                double chargeAmount=chargeFor(lastBillDate,date);
-                addBill(date,chargeAmount);
+
+            protected void addBill(Date date, Double amount) {
             }
-            //将上拉函数中使用到的子类方法声明为abstract,在子类中予以特征化实现.
+
+            public void createBill(Date date) {
+                double chargeAmount = chargeFor(lastBillDate, date);
+                addBill(date, chargeAmount);
+            }
+
+            //将上拉函数中使用到的子类方法声明为protected abstract,在子类中予以特征化实现.
             protected abstract double chargeFor(Date start, Date end);
         }
-        class RegularCustomer extends Customer{
-            protected double chargeFor(Date start, Date end){
+
+        class RegularCustomer extends Customer {
+            protected double chargeFor(Date start, Date end) {
                 return 2.0;
             }
         }
-        class PreferredCustomer extends Customer{
-            protected double chargeFor(Date start, Date end){
+
+        class PreferredCustomer extends Customer {
+            protected double chargeFor(Date start, Date end) {
                 return 1.0;
             }
         }
