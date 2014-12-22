d54beb76b6ae8b2e9fd5687514b46ba654a391c6
filber <filber@foxmail.com>
2014-12-22 17:25:14 +0800

    Update Separate Domain From Presentation.

--- Refactor/src/com/filber/refactor/_7_huge/_71_SeparateDomainFromPresentation_395.java
+++ Refactor/src/com/filber/refactor/_7_huge/_71_SeparateDomainFromPresentation_395.java
@@ -1,15 +1,26 @@
 package com.filber.refactor._7_huge;
 
+/**
+ * 将领域和表述/显示分离
+ */
 public class _71_SeparateDomainFromPresentation_395 {
-    class BadCase{
-        class OrderWindow{}
-    }
 
-    class GoodCase{
-        class OrderWindow{}
+    class BadCase {
+        class OrderWindow {
+        }
+    }
+    //------------------------------------------------------------------
+    //MVC最核心的价值:将用户界面代码和领域模型分离.
+    class GoodCase {
+        class OrderWindow {
+        }
+
         //Introduce Domain Object.
         //Separate Domain From Presentation.
-        class Order{}
-        class OrderLine{}
+        class Order {
+        }
+
+        class OrderLine {
+        }
     }
 }
