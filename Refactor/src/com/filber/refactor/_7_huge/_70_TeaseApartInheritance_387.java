7751fefd4b8fa41c20ec3a564e0539049f54a12a
filber <filber@foxmail.com>
2014-12-22 17:22:41 +0800

    Update Tease Apart Inheritance.

--- Refactor/src/com/filber/refactor/_7_huge/_70_TeaseApartInheritance_387.java
+++ Refactor/src/com/filber/refactor/_7_huge/_70_TeaseApartInheritance_387.java
@@ -1,24 +1,35 @@
 package com.filber.refactor._7_huge;
 
+/**
+ * 梳理并分解继承体系
+ */
 public class _70_TeaseApartInheritance_387 {
 
     class BadCase{
+        //Deal<--ActiveDeal&PassiveDeal
+        //ActiveDeal<--TabularActiveDeal
+        //PassiveDeal<--TabularPassiveDeal
         class Deal{}
         class ActiveDeal extends Deal{}
         class PassiveDeal extends Deal{}
         class TabularActiveDeal extends ActiveDeal{}
+        class SingleActiveDeal extends ActiveDeal{}
         class TabularPassiveDeal extends PassiveDeal{}
+        class SinglePassiveDeal extends PassiveDeal{}
     }
 
+    //------------------------------------------------------------------
     class GoodCase{
+        //Deal<--ActiveDeal&PassiveDeal
         class Deal{
             private PresentationStyle presentationStyle;
         }
         class ActiveDeal extends Deal{}
         class PassiveDeal extends Deal{}
 
+        //PresentationStyle<--SingleStyle&TabularStyle+
         class PresentationStyle{}
-        class SinglePresentationStyle extends PresentationStyle{}
-        class TabularPresentationStyle extends PresentationStyle{}
+        class SingleStyle extends PresentationStyle{}
+        class TabularStyle extends PresentationStyle{}
     }
 }
