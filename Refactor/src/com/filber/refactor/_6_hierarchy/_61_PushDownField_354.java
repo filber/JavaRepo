112d1838bc408eb07b11e9eadf2e6913ed26eb6c
filber <filber@foxmail.com>
2014-12-22 15:57:40 +0800

    Update Pull Down Field.

--- Refactor/src/com/filber/refactor/_6_hierarchy/_61_PushDownField_354.java
+++ Refactor/src/com/filber/refactor/_6_hierarchy/_61_PushDownField_354.java
@@ -1,17 +1,24 @@
 package com.filber.refactor._6_hierarchy;
 
+/**
+ * 字段下移
+ */
 public class _61_PushDownField_354 {
-    class OldCase{
-        class Employee{
+    class BadCase {
+        class Employee {
             protected int quota;
         }
-        class Salesman extends Employee{}
+
+        class Salesman extends Employee {
+        }
     }
 
-    class GoodCase{
-        class Employee{
+    //------------------------------------------------------------------
+    class GoodCase {
+        class Employee {
         }
-        class Salesman extends Employee{
+
+        class Salesman extends Employee {
             //将不能被全部子类使用到的属性下移.
             protected int quota;
         }
