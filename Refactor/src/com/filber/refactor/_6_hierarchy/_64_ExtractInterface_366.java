08c9c21ac7ef6d8f812b286d396fd8233a56058f
filber <filber@foxmail.com>
2014-12-22 16:41:37 +0800

    Update Extract Interface.

--- Refactor/src/com/filber/refactor/_6_hierarchy/_64_ExtractInterface_366.java
+++ Refactor/src/com/filber/refactor/_6_hierarchy/_64_ExtractInterface_366.java
@@ -1,64 +1,74 @@
 package com.filber.refactor._6_hierarchy;
 
 /**
- * Created by Administrator on 2014/12/10.
+ * 提炼接口
  */
 public class _64_ExtractInterface_366 {
-    class OldCase{
-        class Employee{
-            int getRate(){
+    class BadCase {
+        class Employee {
+            int getRate() {
                 return 10;
             }
-            boolean hasSpecialSkill(){
+
+            boolean hasSpecialSkill() {
                 return true;
             }
         }
-        class Computer{
-            int getRate(){
+
+        class Computer {
+            int getRate() {
                 return 15;
             }
-            boolean hasSpecialSkill(){
+
+            boolean hasSpecialSkill() {
                 return false;
             }
         }
-        double charge(Employee employee,int days){
-            int base = employee.getRate()*days;
-            if (employee.hasSpecialSkill())base*=1.5;
+
+        double charge(Employee employee, int days) {
+            int base = employee.getRate() * days;
+            if (employee.hasSpecialSkill()) base *= 1.5;
             return base;
         }
-        double charge(Computer computer,int days){
-            int base = computer.getRate()*days;
-            if (computer.hasSpecialSkill())base*=1.5;
+
+        double charge(Computer computer, int days) {
+            int base = computer.getRate() * days;
+            if (computer.hasSpecialSkill()) base *= 1.5;
             return base;
         }
     }
 
     //-------------------------------------------------------------------------------------------------
-
-    static class NewCase{
-        interface Billable{
+    static class GoodCase {
+        interface Billable {
             int getRate();
+
             boolean hasSpecialSkill();
         }
-        class Employee implements Billable{
-            public int getRate(){
+
+        class Employee implements Billable {
+            public int getRate() {
                 return 10;
             }
-            public boolean hasSpecialSkill(){
+
+            public boolean hasSpecialSkill() {
                 return true;
             }
         }
-        class Computer implements Billable{
-            public int getRate(){
+
+        class Computer implements Billable {
+            public int getRate() {
                 return 15;
             }
-            public boolean hasSpecialSkill(){
+
+            public boolean hasSpecialSkill() {
                 return false;
             }
         }
-        double charge(Billable billable,int days){
-            int base = billable.getRate()*days;
-            if (billable.hasSpecialSkill())base*=1.5;
+
+        double charge(Billable billable, int days) {
+            int base = billable.getRate() * days;
+            if (billable.hasSpecialSkill()) base *= 1.5;
             return base;
         }
     }
