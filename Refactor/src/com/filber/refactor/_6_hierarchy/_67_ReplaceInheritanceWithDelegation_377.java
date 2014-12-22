22dd6bceb5a2e1bea879a2a64b2cca4b78c50904
filber <filber@foxmail.com>
2014-12-22 16:54:34 +0800

    Update Replace Inheritance with Delegation.

--- Refactor/src/com/filber/refactor/_6_hierarchy/_67_ReplaceInheritanceWithDelegation_377.java
+++ Refactor/src/com/filber/refactor/_6_hierarchy/_67_ReplaceInheritanceWithDelegation_377.java
@@ -3,12 +3,37 @@ package com.filber.refactor._6_hierarchy;
 import java.util.EmptyStackException;
 import java.util.Vector;
 
+/**
+ * 以委托取代继承体系
+ */
 public class _67_ReplaceInheritanceWithDelegation_377 {
+    //子类只使用了超类接口中的一部分,或是根本不需要继承而来的数据.
 
-    //    class MyStack<E> extends Vector<E>{
-    class MyStack<E>{
-        //        private Vector<E> vector= this;
-        private Vector<E> vector= new Vector<E>();
+    class InheritanceStack<E> extends Vector<E> {
+        public E push(E item) {
+            super.addElement(item);
+            return item;
+        }
+
+        public synchronized E pop() {
+            E obj;
+            int len = super.size();
+            obj = peek();
+            super.removeElementAt(len - 1);
+            return obj;
+        }
+
+        public synchronized E peek() {
+            int len = super.size();
+            if (len == 0)
+                throw new EmptyStackException();
+            return super.elementAt(len - 1);
+        }
+    }
+
+    //------------------------------------------------------------------
+    class DelegationStack<E> {
+        private Vector<E> vector = new Vector<E>();
 
         public E push(E item) {
             vector.addElement(item);
@@ -30,5 +55,4 @@ public class _67_ReplaceInheritanceWithDelegation_377 {
             return vector.elementAt(len - 1);
         }
     }
-
 }
