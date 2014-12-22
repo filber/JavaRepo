f19f54cb126ada20b18b1e78df908683a96d89ce
filber <filber@foxmail.com>
2014-12-22 16:42:54 +0800

    Update Collapse Hierarchy.

--- Refactor/src/com/filber/refactor/_6_hierarchy/_65_CollapseHierarchy_369.java
+++ Refactor/src/com/filber/refactor/_6_hierarchy/_65_CollapseHierarchy_369.java
@@ -1,12 +1,22 @@
 package com.filber.refactor._6_hierarchy;
 
+/**
+ * 折叠继承体系
+ */
 public class _65_CollapseHierarchy_369 {
-    class OldCase{
-        class Employee{}
-        class Salesman extends Employee{}
+    //超类和子类并无太大区别
+
+    class BadCase {
+        class Employee {
+        }
+
+        class Salesman extends Employee {
+        }
     }
 
-    class NewCase{
-        class Employee{}
+    //------------------------------------------------------------------
+    class GoodCase {
+        class Employee {
+        }
     }
 }
